66d6e5761fa0866c9add80945826fb5cbaf67074
Aleksandre Kandelaki <aleksandre.kandelaki@osapiens.com>
2025-12-14 17:31:53 -0300

    personalize

--- _make_favicon.sh
+++ _make_favicon.sh
@@ -1,1 +1,5 @@
-convert -background transparent "images/PROFILE.JPEG" -define icon:auto-resize=16,24,32,48,64,72,96,128,256 "favicon.ico"
+#convert -background transparent "images/PROFILE.JPEG" -define icon:auto-resize=16,24,32,48,64,72,96,128,256 "favicon.ico"
+
+
+magick -background transparent "images/PROFILE.jpg" \
+  -define icon:auto-resize=16,24,32,48,64,72,96,128,256 "favicon.ico"
